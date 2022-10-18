37a15aacba980c242f2b0f63cc04e57af7215982
Chad Bates <cbates255@gmail.com>
2022-10-17 21:06:59 -0400

    coded

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "app_server" {
   ami           = var.image_ami
   instance_type = var.instance_type
-
+  associate_public_ip_address = true
+  
   tags = {
     Name = var.tags
   }
